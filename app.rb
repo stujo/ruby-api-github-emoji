3a9195e9d64699e947f5513e92f4b472c6d462ce
stujo <stujo@users.noreply.github.com>
2015-01-23 22:49:12 -0800

    Loading Emojis JSON and rendering

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
+require 'httparty'
 
 get '/' do
+  @api_response = HTTParty.get('https://api.github.com/emojis');
+
+  @emojis = JSON.parse(@api_response.body);
+
   erb :index
 end
 
