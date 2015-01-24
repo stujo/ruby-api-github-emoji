9823323ff290705d13168325e97f5ae92dc71693
stujo <stujo@users.noreply.github.com>
2015-01-23 23:11:48 -0800

    Adding webmock gem to stub api calls and testing stubbed data

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
-describe 'Home Page' do 
+describe 'Home Page' do
+
+  let(:emojis_from_github){
+
+    {
+      "+1" => "https://github.global.ssl.fastly.net/images/icons/emoji/+1.png?v5",
+      "-1" => "https://github.global.ssl.fastly.net/images/icons/emoji/-1.png?v5",
+      "100" => "https://github.global.ssl.fastly.net/images/icons/emoji/100.png?v5",
+      "1234" => "https://github.global.ssl.fastly.net/images/icons/emoji/1234.png?v5",
+      "8ball" => "https://github.global.ssl.fastly.net/images/icons/emoji/8ball.png?v5",
+      "a" => "https://github.global.ssl.fastly.net/images/icons/emoji/a.png?v5",
+      "ab" => "https://github.global.ssl.fastly.net/images/icons/emoji/ab.png?v5"
+    }
+  }
+
+  before(:each) do
+    stub_request(:get, /api.github.com\/emojis/).
+      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: emojis_from_github.to_json , headers: {})
+  end
+
   it 'should be ok' do
     expect(get '/').to be_ok
-  end  
+  end
 
   it 'should contain emojis from github api' do
     get '/'
-    expect(last_response.body).to include('https://assets-cdn.github.com/images/icons/emoji/unicode/1f600.png?v5')
+    expect(last_response.body).to include('src="https://github.global.ssl.fastly.net/images/icons/emoji/1234.png?v5"')
   end
 end
