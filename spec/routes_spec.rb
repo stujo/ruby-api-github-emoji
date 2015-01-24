f2a3329a8c53bfb6806d23ca1f7e1c3b35a836cb
stujo <stujo@users.noreply.github.com>
2015-01-23 23:09:58 -0800

    Adding rspec without stubbing

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -4,4 +4,9 @@ describe 'Home Page' do
   it 'should be ok' do
     expect(get '/').to be_ok
   end  
+
+  it 'should contain emojis from github api' do
+    get '/'
+    expect(last_response.body).to include('https://assets-cdn.github.com/images/icons/emoji/unicode/1f600.png?v5')
+  end
 end
